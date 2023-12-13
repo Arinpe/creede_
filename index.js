2bb29d7cda0b8fce5084a0d9a2bebc4284a8edfd
Arinpe <dehtolah@gmail.com>
2023-12-13 11:56:39 +0100

    landing page

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const searchForm = document.getElementById('searchForm');
+// generate DID
 
-    searchForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevent the default form submission behavior
-
-        const searchInput = document.querySelector('#searchForm input[name="search"]');
-        const searchQuery = searchInput.value.trim();
-
-        // Perform your search logic with the searchQuery
-        console.log('Search query:', searchQuery);
-
-        // Clear the input field after logging the search query
-        searchInput.value = '';
-    });
-});
+const createDID = async () => {
+    const { Web5 } = await import("@web5/api/browser");
+    const { web5, did } = await Web5.connect();
+    // the did here would be the user's device did;
+    setUserDid(did);
+  };
+  // create a custom hook to always stay connected
+  
+  const useWeb5 = () => {
+    const [web5, setWeb5] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [did, setDid] = useState("");
+  
+    useEffect(() => {
+      const initWeb5 = async () => {
+        setLoading(true);
+        // @ts-ignore
+        const { Web5 } = await import("@web5/api/browser");
+        try {
+          const { web5, did } = await Web5.connect();
+          setWeb5(web5);
+          setDid(did);
+          setLoading(false);
+        } catch (error) {
+          console.error("Error initializing Web5", error);
+          setLoading(false);
+          setError();
+        }
+      };
+  
+      initWeb5();
+    }, []);
+    return { web5, did, loading, error };
+  };
+  // const {web5} = useWeb5()
+  const createAccount = async () => {
+    const profile = {
+      fullName: "",
+      institutionName: "",
+      positionAtInstitution: "",
+    };
+  
+    try {
+      const { record } = await web5.dwn.records.create({
+        data: profile,
+        message: {
+          schema: "http://example.com/profile",
+          dataFormat: "application/json",
+        },
+      });
+      setLoading(false);
+  
+      // redirect if successful? 
+    } catch (error) {
+      console.error("Error creating DID:", error);
+      setLoading(false);
+      setError(true);
+    }
+  };
+  
